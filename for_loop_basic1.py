1c3e8e09f70a372a1a955b5f804431d777a89e86
Wade Hunt <wade.hunt91@gmail.com>
2022-10-14 13:12:30 -0700

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,33 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range (5,1001,5):
+#     if i% 5 == 0 :
+#         print(i)
+
+# for i in range(1,101):
+#     if i % 5 == 0:
+#         if i % 10 ==0:
+#             print("Coding dojo")
+#             continue
+#         print("coding")
+#     else:
+#         print(i)
+
+# sum = 0
+# for i in range(0,500000):
+#     sum = sum + i
+# print(sum)
+
+# i = 2018
+# while i > -1 :
+#     print(i)
+#     i -= 4
+
+# lowNum = 2
+# highNum = 12
+# mult = 4
+# for i in range(lowNum,(highNum+1)):
+#     if i % mult == 0:
+#         print(i)
+
